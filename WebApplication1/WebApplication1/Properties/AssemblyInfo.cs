adce69bf6e946a84e684b60eeaef63dda5043436
Hook25 <massigi@live.it>
2015-09-18 08:13:27 +0200

    First commit

---
+++ WebApplication1/WebApplication1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("WebApplication1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApplication1")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili 
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da 
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato per creare l'ID della libreria dei tipi
+[assembly: Guid("d0fb089d-0a7b-49d1-a784-d27d305eb8af")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai quattro valori seguenti:
+//
+//      Versione principale
+//      Versione secondaria 
+//      Numero build
+//      Revisione
+//
+// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build 
+// utilizzando l'asterisco (*) come illustrato di seguito:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebApplication1/WebApplication1/script.js
@@ -0,0 +1,113 @@
+var imgDB, intSelected;
+var canvas, ctx
+var boolDraw;
+var functionList;
+var inUseFunction;
+var extractImg;
+
+function init(){
+	var div = document.getElementById("img");
+	intSelected=0;
+	functionList= loadFunction();
+	imgDB = loadPhoto();
+	extractImg = defaultImg;
+	if(imgDB==undefined || imgDB.length==0){
+		console.error("Error occurred in init. Operation aborted");
+		return;
+	}
+	inUseFunction = function() {};
+	canvas=document.getElementById("canvas");
+	canvas.addEventListener("click",click);
+	document.addEventListener("mousedown",function(){boolDraw=true;});
+	document.addEventListener("mouseup",function(){boolDraw=false;});
+	canvas.addEventListener("mousemove",mousemove);
+	ctx=canvas.getContext('2d');
+	ctx.canvas.width = window.innerWidth * 0.95;
+	ctx.canvas.height = window.innerHeight * 0.95;
+	menu();
+	boolDraw=false;	
+}
+function mousemove(e){
+	if( boolDraw && e.clientY>80 && e.clientX >80 )
+	{ 
+		inUseFunction(); 
+		console.log(extractImg);
+		ctx.drawImage(imgDB[extractImg(intSelected, imgDB.length)],e.clientX-20,e.clientY-20,40,40);
+	}
+}
+
+function defaultImg(i){ return i; }
+
+function click(e){
+	var spaceB = canvas.width/imgDB.length;
+	//menu superiore
+	if(e.clientY<60){
+		for(var i = 0; i< imgDB.length;i++){
+			if(e.clientX>=imgDB[i].xPos && e.clientX <=imgDB[i].xPos+40){
+				intSelected=i;
+				inUseFunction = function() { }
+				extractImg = defaultImg;
+				return;
+			}
+		}
+	}else if(e.clientX < 60){
+		for(var i = 0; i<functionList.length; i++){
+			if(e.clientX >= functionList[i].xPos && e.clientX <=functionList[i].xPos+40){
+				inUseFunction = window[functionList[i].dataset.todo];
+				return;
+			}
+		}
+	}
+}
+function loadFunction(){
+	var toR = new Array();
+	var id = 0;
+	var output = document.getElementById("functions" + id);
+	
+	while(output != undefined){
+		toR.push(output);
+		id++;
+		output=document.getElementById("functions" + id);
+	}
+	
+	return toR;
+}
+function loadPhoto(){
+	var toR= new Array();
+	var id=0;
+	var output = document.getElementById("foto" + id);
+
+	while(output!=undefined){
+		toR.push(output);
+		id++;
+		output=document.getElementById("foto" + id);
+	}
+	return toR;
+}
+function menu(){
+	supMen();
+	rightMen();
+}
+function supMen(){
+	var spaceB = (canvas.width-40)/(imgDB.length-1);
+	ctx.beginPath();
+    ctx.rect(0, 0, canvas.width, 60);
+    ctx.fillStyle = 'red';
+	ctx.fill();
+	for(var i = 0; i<imgDB.length; i++){
+		imgDB[i].xPos=i*spaceB;
+		ctx.drawImage(imgDB[i],i*spaceB + 3,10,40,40);
+	}
+}
+function rightMen(){
+	var spaceR = (canvas.height - 40) / functionList.length;
+	ctx.beginPath();
+	ctx.rect(0,60,60,canvas.height);
+	ctx.fillStyle = 'blue';
+	ctx.fill();
+	for(var i = 0; i<functionList.length; i++){
+		functionList[i].xPos = spaceR * i;
+		ctx.drawImage(functionList[i],10,70 + (i*spaceR + 3),40,40);
+	}
+	//10 70
+}
