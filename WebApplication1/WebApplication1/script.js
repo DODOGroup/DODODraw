5b43371ec851d7276682c0f62de8f6754daf7d52
SuperBonny <bonora.matteo@gmail.com>
2015-09-22 22:59:04 +0200

    Added html menus

--- WebApplication1/WebApplication1/script.js
+++ WebApplication1/WebApplication1/script.js
@@ -4,6 +4,7 @@ var boolDraw;
 var functionList;
 var inUseFunction;
 var extractImg;
+var offset;
 
 function init(){
 	var div = document.getElementById("img");
@@ -22,9 +23,9 @@ function init(){
 	document.addEventListener("mouseup",function(){boolDraw=false;});
 	canvas.addEventListener("mousemove", mousemove);
 	ctx=canvas.getContext('2d');
+	offset  = getOffset(canvas);
 	ctx.canvas.width = window.innerWidth;
 	ctx.canvas.height = window.innerHeight;
-	menu();
 	boolDraw=false;	
 }
 function mousemove(e){
@@ -32,27 +33,19 @@ function mousemove(e){
 	{ 
 		inUseFunction(); 
 		console.log(extractImg);
-		ctx.drawImage(imgDB[extractImg(intSelected, imgDB.length)],e.clientX-20,e.clientY-20,40,40);
+		ctx.drawImage(imgDB[extractImg(intSelected, imgDB.length)],(e.clientX-offset.left)-20,(e.clientY-offset.top)-20,40,40);
 	}
 }
 
-function defaultImg(i){ return i; }
+function defaultImg(i) { return i; }
+function setSelected(i) { intSelected = i; }
 
 function click(e){
 	var spaceB = canvas.width/imgDB.length;
 	//menu superiore
-	if(e.clientY<60) {
-		for(var i = 0; i< imgDB.length;i++){
-			if(e.clientX>=imgDB[i].xPos && e.clientX <=imgDB[i].xPos+40){
-				intSelected=i;
-				inUseFunction = function() { }
-				extractImg = defaultImg;
-				return;
-			}
-		}
-	}else if(e.clientX < 60){
+	if(e.clientX < 60){
 		for(var i = 0; i<functionList.length; i++){
-			if(e.clientX >= functionList[i].xPos && e.clientX <=functionList[i].xPos+40){
+			if(e.clientX >= functionList[i].xPos-offset.left && e.clientX <=(functionList[i].xPos-offset.top)+40){
 				inUseFunction = window[functionList[i].dataset.todo];
 				return;
 			}
@@ -84,30 +77,17 @@ function loadPhoto(){
 	}
 	return toR;
 }
-function menu(){
-	supMen();
-	rightMen();
-}
-function supMen(){
-	var spaceB = (canvas.width-40)/(imgDB.length-1);
-	ctx.beginPath();
-    ctx.rect(0, 0, canvas.width, 60);
-    ctx.fillStyle = 'red';
-	ctx.fill();
-	for(var i = 0; i<imgDB.length; i++){
-		imgDB[i].xPos=i*spaceB;
-		ctx.drawImage(imgDB[i],i*spaceB + 3,10,40,40);
-	}
-}
-function rightMen(){
-	var spaceR = (canvas.height - 40) / functionList.length;
-	ctx.beginPath();
-	ctx.rect(0,60,60,canvas.height);
-	ctx.fillStyle = 'blue';
-	ctx.fill();
-	for(var i = 0; i<functionList.length; i++){
-		functionList[i].xPos = spaceR * i;
-		ctx.drawImage(functionList[i],10,70 + (i*spaceR + 3),40,40);
-	}
-	//10 70
-}
+
+function getOffset(obj) {
+          var offsetLeft = 0;
+          var offsetTop = 0;
+          do {
+            if (!isNaN(obj.offsetLeft)) {
+                offsetLeft += obj.offsetLeft;
+            }
+            if (!isNaN(obj.offsetTop)) {
+                offsetTop += obj.offsetTop;
+            }   
+          } while(obj = obj.offsetParent );
+          return {left: offsetLeft, top: offsetTop};
+      } 
